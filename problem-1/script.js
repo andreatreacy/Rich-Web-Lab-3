0d0105a07b8567ca12e76059fb184a8f061d7d26
andrea treacy <andreatreacy@gmail.com>
2022-10-21 22:04:54 +0100

    problem 1 filter working

--- problem-1/script.js
+++ problem-1/script.js
@@ -15,61 +15,54 @@ function fillTable(data)
 	
     for(let i=0; i<data.length; i++)
 	{
-		console.log(i);
-		console.log(data[i].name);
-		
 		// insert the new row at the end of the table
 		let row = table.insertRow(-1);
 		
 		let cellpos = 0;
-			var colname = row.insertCell(cellpos);
-			colname.innerHTML = data[i].name;
+		var colname = row.insertCell(cellpos);
+		colname.innerHTML = data[i].name;
 			
-			var colmobile = row.insertCell(cellpos+1);
-			colmobile.innerHTML = data[i].mobile;
+		var colmobile = row.insertCell(cellpos+1);
+		colmobile.innerHTML = data[i].mobile;
 			
-			var colemail = row.insertCell(cellpos+2);
-			colemail.innerHTML = data[i].email;
-		
-		console.log(data[i].mobile);
-		
-		
-		/*
-        let colname = 'name-${i}';
-        let colmobile = 'mobile-${i}';
-        let colemail = 'email-${i}';
-
-        var row = '<tr><td>${data[i].name}</td><td>${data[i].mobile}</td><td>${data[i].email}</td></tr>';
-        table.innerHTML += row;*/
+		var colemail = row.insertCell(cellpos+2);
+		colemail.innerHTML = data[i].email;
     }
 }
 
 
-function myFunction() {
-  // Declare variables
-  var input, filter, table, tr, td, i, txtValue;
-  
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("contactTable");
-  tr = table.getElementsByTagName("tr");
-
-  // Loop through all table rows, and hide those who don't match the search query
-  for(i=0; i<tr.length; i++) 
-  {
-    td = tr[i].getElementsByTagName("td")[0];
-    if(td) 
-	{
-      txtValue = td.textContent || td.innerText;
+function filter() 
+{
+	// Declare variables
+	var input, filter, table, tr, cell, i, cellContent;
 	  
-      if (txtValue.toUpperCase().indexOf(filter) > -1) 
-	  {
-        tr[i].style.display = "";
-      } 
-	  else 
-	  {
-        tr[i].style.display = "none";
-      }
-    }// end outer if
-  }//end for
+	input = document.getElementById("filterInput").value;
+	table = document.getElementById("contactTable");
+	tr = table.getElementsByTagName("tr");
+
+	// loop through the rows in the table and hide the ones that don't match the filter input
+	for(i=0; i<tr.length; i++) 
+	{
+		// only filter by the mobile column
+		cell = tr[i].getElementsByTagName("td")[1];
+		
+		// if cell exists
+		if(cell) 
+		{
+			cellContent = cell.textContent;
+			
+			// display content if it contains the filter input
+			if(cellContent.indexOf(input) > -1) 
+			{
+				tr[i].style.display = "";
+			}
+			// hide content if it does not contain the filter input
+			else 
+			{
+				tr[i].style.display = "none";
+			}
+		}// end outer if
+	}//end for
+  
+
 }
