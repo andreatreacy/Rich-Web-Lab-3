9dfc227ec01a7b8945348771969a0fc2aef9c235
andrea treacy <andreatreacy@gmail.com>
2022-10-22 15:38:08 +0100

    problem 1 finished

--- problem-1/script.js
+++ problem-1/script.js
@@ -5,12 +5,134 @@ let contactsList = [
     {'name':'Paul Brown', 'mobile':'0858887945', 'email':'pbrown@gmail.com'},
 ];
 
+
 fillTable(contactsList);
+
+
+
+
+let addButton = document.getElementById("addButton");
+addButton.addEventListener("click", validate);
+
+function validate() 
+{
+	// Validate the Name
+	
+	let nameInput = document.getElementById("nameInput").value;
+	console.log(nameInput);
+	  
+	console.log(/^[a-zA-Z]+$/.test(nameInput[0]));
+	let nameValid = true;
+	
+	console.log(nameInput.length);
+	// check if name input is not blank and has no more than 20 characters
+	if(nameInput.length <= 20 && nameInput.length != 0)
+	{
+		for(let i=0; i<nameInput.length; i++)
+		{
+			if(/^[a-zA-Z]+$/.test(nameInput[i]) != true && nameInput[i] != " ")
+			{
+				nameValid = false;
+			}
+		}
+	}
+	else
+	{
+		nameValid = false;
+	}
+	
+	
+	// Validate the Mobile Number
+	
+	let mobileInput = document.getElementById("mobileInput").value;
+	let mobileValid = true;
+	
+	console.log(isNaN("e"));
+	if(mobileInput.length == 10)
+	{
+		for(let i=0; i<mobileInput.length; i++)
+		{
+			if(isNaN(mobileInput[i]))
+			{
+				mobileValid = false;
+			}
+		}
+	}
+	else
+	{
+		mobileValid = false;
+	}
+	console.log(">");
+	console.log(mobileValid);
+	
+	
+	// Validate the Email Address
+	
+	let emailInput = document.getElementById("emailInput").value;
+	let emailValid = true;
+	console.log(emailInput.length);
+	
+	// regex for correct email format
+	let emailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+	
+	// if emailInput is not the correct format or is the wrong length it is not valid
+	if(!emailInput.match(emailFormat) || emailInput.length >= 40 || emailInput.length == 0)
+	{
+		emailValid = false;
+	}
+	
+	
+	
+	// if all inputs are valid, add the contact
+	if(nameValid == true && mobileValid == true && emailValid == true)
+	{
+		// hide the error div if inputs are valid
+		let inputError = document.getElementById("error");
+		inputError.style.display = "none";
+		
+		// clear the input fields
+		document.getElementById("nameInput").value = "";
+		document.getElementById("mobileInput").value = "";
+		document.getElementById("emailInput").value = "";
+		
+		// add the new contact
+		addContact(nameInput, mobileInput, emailInput);
+	}
+	// if all inputs are not valid, display the error div
+	else
+	{
+		let inputError = document.getElementById("error");
+		inputError.style.display = "block";
+	}
+}
+
+
+
+function addContact(nameInput, mobileInput, emailInput)
+{
+	console.log("adding");
+	
+	let newContact = {
+		name: nameInput,
+		mobile: mobileInput,
+		email: emailInput
+	};
+	
+	contactsList.push(newContact);
+	fillTable(contactsList);
+	
+}
+
+
+
+
+
+
 
 
 // sort the table by the name column
 function sort(contactsList)
-{
+{	
 	let isSorted = true;
 
 	// check if the table is alread sorted in ascending order
@@ -22,7 +144,7 @@ function sort(contactsList)
 			break;
 		}
 	}
-	
+			
 	// if it is not, sort it in ascending order
 	if(isSorted == false)
 	{
@@ -36,6 +158,17 @@ function sort(contactsList)
 		fillTable(contactsList);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 // remove all the rows from the table except the header row
@@ -77,38 +210,45 @@ function fillTable(contactsList)
 }
 
 
-function filter() 
+
+
+
+function filter(contactsList)
 {
-	// Declare variables
-	var input, filter, table, tr, cell, i, cellContent;
-	  
-	input = document.getElementById("filterInput").value;
-	table = document.getElementById("contactTable");
-	tr = table.getElementsByTagName("tr");
-
-	// loop through the rows in the table and hide the ones that don't match the filter input
-	for(i=0; i<tr.length; i++) 
+	// get the search input
+	let input = document.getElementById("filterInput").value;
+	
+	// empty array to store search matches
+	let resultList = [];
+	
+	// go through contactsList
+	for(let i=0; i<contactsList.length; i++) 
 	{
-		// only filter by the mobile column
-		cell = tr[i].getElementsByTagName("td")[1];
+		let mobile = contactsList[i].mobile;
 		
-		// if cell exists
-		if(cell) 
+		// if the input appears in an element, add the element to resultList
+		if(mobile.includes(input))
 		{
-			cellContent = cell.textContent;
-			
-			// display content if it contains the filter input
-			if(cellContent.indexOf(input) > -1) 
-			{
-				tr[i].style.display = "";
-			}
-			// hide content if it does not contain the filter input
-			else 
-			{
-				tr[i].style.display = "none";
-			}
-		}// end outer if
-	}//end for
-  
-
+			resultList.push(contactsList[i]);
+		}
+		
+		// if no results are found, display the noResult div
+		if(resultList.length === 0)
+		{
+			let noResult = document.getElementById("noResult");
+			noResult.style.display = "block";
+		}
+		// if results are found, hide the noResult div
+		else
+		{
+			let noResult = document.getElementById("noResult");
+			noResult.style.display = "none";
+		}
+	}
+	
+	// fill the table with the search results
+	fillTable(resultList);
 }
+
+
+
